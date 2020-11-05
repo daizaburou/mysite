3fa8f8bcab1c8942b97e4004e770674fb9ab1a03
daizaburo <daizabro@gmail.com>
2020-11-06 00:01:57 +0900

    トグルを追加

--- common/js/common.js
+++ common/js/common.js
@@ -1,1 +1,13 @@
-//JS
+//スライドトグル
+(function($){
+    $('.toggle-btn').click(function(){
+    $('.toggle').slideToggle(200);
+    if($('.toggle-btn').text() === "続きを読む"){
+        $('.toggle-btn').text("省略する");
+        }
+        else{
+        $('.toggle-btn').text("続きを読む");
+        }
+    
+    });
+    })(jQuery);
