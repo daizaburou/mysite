e7e99e5e41c4e9c8ee5b1197c19e34b7ec76328e
daizaburo <daizabro@gmail.com>
2020-11-18 00:29:51 +0900

    トグルを汎用的に使用できるように改良

--- common/js/common.js
+++ common/js/common.js
@@ -1,15 +1,18 @@
 (function($){
 //スライドトグル
-    $('.toggle-btn').click(function(){
-    $('.toggle').slideToggle(200);
-    if($('.toggle-btn').text() === "続きを読む"){
-        $('.toggle-btn').text("省略する");
+$('.toggle-btn-inner').each(function(){
+  $(this).on('click',function(){
+    let target = $(this).data('target');
+    let toggle = document.getElementById(target);
+    $(toggle).slideToggle(200);
+    if($(this).text() === "続きを読む"){
+      $(this).text("省略する");
         }
         else{
-        $('.toggle-btn').text("続きを読む");
+          $(this).text("続きを読む");
         }
-    
     });
+  });
 
 //ファーストビュー
       if( !sessionStorage.getItem('first-view') ) {
