dd313542ba7bf233828c1f0c1b5bbd1c7bf80a5d
daizaburo <daizabro@gmail.com>
2020-12-20 23:49:08 +0900

    フォーム追加

--- common/js/common.js
+++ common/js/common.js
@@ -46,4 +46,14 @@ slideToggle();
         });
     });
 
+
+    $(function() {
+      let tabs = $(".tab");
+      $(".tab").on("click", function() {
+        $(".active").removeClass("active");
+        $(this).addClass("active");
+        const index = tabs.index(this);
+        $(".tab-inner").removeClass("open").eq(index).addClass("open");
+      })
+    })
     })(jQuery);
