c1551a3adcb4da0a53ae17676d2edcd4309610af
daizaburo <daizabro@gmail.com>
2021-01-05 23:40:36 +0900

    ハンバーガーを調整

--- common/js/init.js
+++ common/js/init.js
@@ -9,8 +9,15 @@ $('.slider').slick({
     {
       breakpoint: 768,
       settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
         slidesToShow: 1,
-        slidesToScroll: 1,
+        slidesToScroll: 1
       }
     }
   ]
