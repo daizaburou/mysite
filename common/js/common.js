9050f7e12662feeeebd4f4e57119126c0d6a626e
daizaburo <daizabro@gmail.com>
2021-01-07 23:05:47 +0900

    トップをレスポンシブ化

--- common/js/common.js
+++ common/js/common.js
@@ -73,6 +73,11 @@ $(function(){
   $('.btn-hamb').on('click', function() {
     $(this).toggleClass('active');
     $(".header-inner").toggleClass('active');
+    setTimeout(function(){
+      $(".global-nav").toggleClass('active');
+    }
+    ,500
+    );
     return false;
   });
 });
