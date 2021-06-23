19f1cb22835afc0663d1dfd9be3a79e3a56d6c75
d_fujimoto <d_fujimoto@beeworks.co.jp>
2021-06-23 14:59:51 +0900

    gullpfile作成途中

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,12 @@
 'use strict';
-const path = require('path');
 const gulp = require('gulp');
-const fs = require('fs-extra');
-const mkdirp = require('mkdirp');
-const csvSync = require('csv-parse/lib/sync');
-const stringify = require('json-stable-stringify');
-const pug = require('gulp-pug');
 const sass = require('gulp-sass');
-const sassGlob = require('gulp-sass-glob');
-const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const mqpacker = require('css-mqpacker');
-const cleanCss = require('gulp-clean-css');
 const webpackStream = require('webpack-stream');
 const webpack = require('webpack');
 const plumber = require('gulp-plumber');
 const browserSync = require('browser-sync').create();
-const packageImporter = require('node-sass-package-importer');
 const glob = require('glob');
 
 const config = {
@@ -35,93 +25,6 @@ const config = {
   },
   baseDir: './dist', // browser-syncのbaseDir
 };
-
-class Utility {
-  static writeFileSync(filepath, contents, callback) {
-    mkdirp.sync(path.dirname(filepath));
-    fs.writeFileSync(filepath, contents, callback);
-  }
-}
-
-// CSV→JSON
-function exportJson(done) {
-  const dirName = 'lang';
-  fs.readdirSync(__dirname + `/src/${dirName}/`).forEach((filename) => {
-    const data = csvSync(fs.readFileSync(__dirname + `/src/${dirName}/${filename}`));
-    const fileBaseName = path.basename(filename, '.csv');
-    const json = {};
-
-    const objName = {
-      area: '',
-      prop: '',
-    };
-
-    const firstRow = data[0];
-
-    for (let col = 2; col < firstRow.length; col++) {
-      json[firstRow[col]] = {};
-    }
-
-    for (let row = 1; row < data.length; row++) {
-      const thisArea = data[row][0];
-      if (thisArea) objName.area = thisArea;
-      objName.prop = data[row][1];
-      for (let col = 2; col < data[row].length; col++) {
-        if (!json[firstRow[col]][objName.area]) json[firstRow[col]][objName.area] = {};
-        json[firstRow[col]][objName.area][objName.prop] = data[row][col].replace(/(\r)?\n/g, '<br>');
-      }
-    }
-    // console.log('file:', filename);
-    // console.log('json:', json);
-    Utility.writeFileSync(`src/pug/_data/${dirName}/${fileBaseName}.json`, stringify(json, { space: '  ' }));
-  });
-  done();
-}
-
-// HTML
-function buildPug(done) {
-  const languages = require('./src/pug/_data/languages');
-  const pages = require('./src/pug/_data/pages');
-
-  for (const template in pages) {
-    const templateDir = path.dirname(template);
-    for (const destPath of pages[template]) {
-      gulp
-        .src(`${config.srcDir}/${config.src.html}/_templates/${template}`)
-        .pipe(plumber())
-        .pipe(
-          pug({
-            pretty: 2,
-            locals: {
-              fs: fs,
-              langId: destPath.lang === 'others' ? languages.default.lang : destPath.lang,
-              destPath: destPath,
-              langList: languages.langList,
-              thisPageLanguages: pages[template],
-              defaultLang: languages.default,
-            },
-          })
-        )
-        .pipe(gulp.dest(`${config.destDir}/${destPath.area}/${destPath.lang}/${templateDir}`));
-    }
-  }
-  gulp
-    .src([`${config.srcDir}/${config.src.html}/[^_]**/[^_]*.pug`, `${config.srcDir}/${config.src.html}/[^_]*.pug`])
-    .pipe(plumber())
-    .pipe(
-      pug({
-        pretty: 2,
-        locals: {
-          fs: fs,
-          langId: languages.default.lang,
-          thisPageLanguages: languages.langList,
-          langList: languages.langList,
-        },
-      })
-    )
-    .pipe(gulp.dest(`${config.destDir}/${config.dest.html}`));
-  done();
-}
 
 // CSS
 function buildScss() {
@@ -205,4 +108,4 @@ exports['build'] = gulp.parallel(gulp.series(exportJson, buildPug), buildScss, b
 exports['build:pug'] = gulp.series(exportJson, buildPug);
 exports['build:scss'] = buildScss;
 exports['build:js'] = buildJs;
-exports['watch'] = gulp.parallel(sync, watch);
+exports['watch'] = gulp.parallel(sync);
