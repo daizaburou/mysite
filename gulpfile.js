66ef4fcd79aa052f3d4d321c8ef2c5bcd03437b3
d_fujimoto <d_fujimoto@beeworks.co.jp>
2021-06-24 18:19:04 +0900

    EJSの構築途中

--- gulpfile.js
+++ gulpfile.js
@@ -2,25 +2,23 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const sassGlob = require('gulp-sass-glob');
-
-// const path = require('path');
-// const gulp = require('gulp');
-// const fs = require('fs-extra');
-// const mkdirp = require('mkdirp');
-// const csvSync = require('csv-parse/lib/sync');
-// const stringify = require('json-stable-stringify');
-// const pug = require('gulp-pug');
-// const CacheBuster = require('gulp-cachebust');
-// const sass = require('gulp-sass');
-// const sassGlob = require('gulp-sass-glob');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const mqpacker = require('css-mqpacker');
 const cleanCss = require('gulp-clean-css');
+const ejs = require('gulp-ejs');
+const rename = require('gulp-rename');
+const browserSync = require('browser-sync').create();
+// const path = require('path');
+// const gulp = require('gulp');
+// const fs = require('fs-extra');
+// const mkdirp = require('mkdirp');
+// const stringify = require('json-stable-stringify');
+// const sass = require('gulp-sass');
+// const sassGlob = require('gulp-sass-glob');
 // const webpackStream = require('webpack-stream');
 // const webpack = require('webpack');
 // const plumber = require('gulp-plumber');
-const browserSync = require('browser-sync').create();
 // const packageImporter = require('node-sass-package-importer');
 // const glob = require('glob');
 
@@ -28,10 +26,9 @@ const browserSync = require('browser-sync').create();
 const config = {
   srcDir: './src',
   src: {
-    html: 'pug',
+    html: 'ejs',
     css: 'scss',
     js: 'js',
-    csv: 'lang',
   },
   destDir: './dist',
   dest: {
@@ -41,6 +38,13 @@ const config = {
   },
   baseDir: './dist', // browser-syncのbaseDir
 };
+
+// EJS
+
+function buildEjs() {
+  return gulp
+  .src(`${config.srcDir}/${config.src.html}/*.ejs`,`!${config.srcDir}/${config.src.html}/**/_*.ejs`)//_付きは読み込まない
+}
 
 // CSS
 function buildScss() {
@@ -108,10 +112,9 @@ function reload(done) {
   done();
 }
 function watch() {
-//   gulp.watch(`${config.srcDir}/${config.src.html}/**/*`, gulp.series(buildPug, reload));
+//   gulp.watch(`${config.srcDir}/${config.src.html}/**/*`, gulp.series(buildEjs, reload));
   gulp.watch(`${config.srcDir}/${config.src.css}/**/*`, gulp.series(buildScss, reload));
 //   gulp.watch(`${config.srcDir}/${config.src.js}/**/*`, gulp.series(buildJs, reload));
-//   gulp.watch(`${config.srcDir}/${config.src.csv}/**/*`, gulp.series(exportJson, buildPug, reload));
 }
 
 // exports['production'] = gulp.series(gulp.parallel(exportJson, buildScss, buildJs), getHash, saveHash, buildPug);
