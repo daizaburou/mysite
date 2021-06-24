1eb6656242f9c53132a833254e1ad8883c75e7c4
d_fujimoto <d_fujimoto@beeworks.co.jp>
2021-06-24 16:53:04 +0900

    環境構築修正

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,6 @@ const webpackStream = require('webpack-stream');
 const webpack = require('webpack');
 const plumber = require('gulp-plumber');
 const browserSync = require('browser-sync').create();
-const glob = require('glob');
 
 const config = {
   srcDir: './src',
@@ -45,67 +44,67 @@ function buildScss() {
     .pipe(gulp.dest(`${config.destDir}/${config.dest.css}`));
 }
 
-// JS
-function buildJs() {
-  const entries = glob.sync('*.js', { cwd: `${config.srcDir}/${config.src.js}` }).map(function (key) {
-    return [key, path.resolve(`${config.srcDir}/${config.src.js}`, key)];
-  });
-  const entryObj = Object.fromEntries(entries);
-  return webpackStream(
-    {
-      mode: 'production',
-      entry: entryObj,
-      output: {
-        filename: '[name]',
-      },
-      plugins: [
-        new webpack.ProvidePlugin({
-          Promise: 'es6-promise',
-        }),
-      ],
-      module: {
-        rules: [
-          {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-              loader: 'babel-loader',
-            },
-          },
-        ],
-      },
-    },
-    webpack
-  ).pipe(gulp.dest(`${config.destDir}/${config.dest.js}`));
-}
+// // JS
+// function buildJs() {
+//   const entries = glob.sync('*.js', { cwd: `${config.srcDir}/${config.src.js}` }).map(function (key) {
+//     return [key, path.resolve(`${config.srcDir}/${config.src.js}`, key)];
+//   });
+//   const entryObj = Object.fromEntries(entries);
+//   return webpackStream(
+//     {
+//       mode: 'production',
+//       entry: entryObj,
+//       output: {
+//         filename: '[name]',
+//       },
+//       plugins: [
+//         new webpack.ProvidePlugin({
+//           Promise: 'es6-promise',
+//         }),
+//       ],
+//       module: {
+//         rules: [
+//           {
+//             test: /\.js$/,
+//             exclude: /node_modules/,
+//             use: {
+//               loader: 'babel-loader',
+//             },
+//           },
+//         ],
+//       },
+//     },
+//     webpack
+//   ).pipe(gulp.dest(`${config.destDir}/${config.dest.js}`));
+// }
 
-function getHash() {
-  return gulp
-    .src([`${config.destDir}/${config.dest.css}/*`, `${config.destDir}/${config.dest.js}/*`], {
-      base: `${config.destDir}`,
-    })
-}
+// function getHash() {
+//   return gulp
+//     .src([`${config.destDir}/${config.dest.css}/*`, `${config.destDir}/${config.dest.js}/*`], {
+//       base: `${config.destDir}`,
+//     })
+// }
 
-// ホットリロード
-function sync() {
-  browserSync.init({
-    server: config.baseDir,
-  });
-}
-function reload(done) {
-  browserSync.reload();
-  done();
-}
-function watch() {
-  gulp.watch(`${config.srcDir}/${config.src.html}/**/*`, gulp.series(buildPug, reload));
-  gulp.watch(`${config.srcDir}/${config.src.css}/**/*`, gulp.series(buildScss, reload));
-  gulp.watch(`${config.srcDir}/${config.src.js}/**/*`, gulp.series(buildJs, reload));
-  gulp.watch(`${config.srcDir}/${config.src.csv}/**/*`, gulp.series(exportJson, buildPug, reload));
-}
+// // ホットリロード
+// function sync() {
+//   browserSync.init({
+//     server: config.baseDir,
+//   });
+// }
+// function reload(done) {
+//   browserSync.reload();
+//   done();
+// }
+// function watch() {
+//   gulp.watch(`${config.srcDir}/${config.src.html}/**/*`, gulp.series(buildPug, reload));
+//   gulp.watch(`${config.srcDir}/${config.src.css}/**/*`, gulp.series(buildScss, reload));
+//   gulp.watch(`${config.srcDir}/${config.src.js}/**/*`, gulp.series(buildJs, reload));
+//   gulp.watch(`${config.srcDir}/${config.src.csv}/**/*`, gulp.series(exportJson, buildPug, reload));
+// }
 
-exports['production'] = gulp.series(gulp.parallel(exportJson, buildScss, buildJs), getHash, saveHash, buildPug);
-exports['build'] = gulp.parallel(gulp.series(exportJson, buildPug), buildScss, buildJs);
-exports['build:pug'] = gulp.series(exportJson, buildPug);
+// exports['production'] = gulp.series(gulp.parallel(exportJson, buildScss, buildJs), getHash, saveHash, buildPug);
+// exports['build'] = gulp.parallel(gulp.series(exportJson, buildPug), buildScss, buildJs);
+// exports['build:pug'] = gulp.series(exportJson, buildPug);
 exports['build:scss'] = buildScss;
-exports['build:js'] = buildJs;
-exports['watch'] = gulp.parallel(sync);
+// exports['build:js'] = buildJs;
+// exports['watch'] = gulp.parallel(sync);
--- readme.md
+++ readme.md
@@ -1,28 +1,13 @@
-# 命名ルール
-クラス名はケバブケース
-▼例
-class="main-vis",class="global-nav"
-
-cssの装飾はclass、js、アンカーで活用する場合はidを用いる。
-マージンは下につけるようにする
-
 ## 環境
-node: 12.4.0
-npm: 6.9.0
+node: 12.5.0
+npm: 6.14.5
 VSCode
 
 ## ディレクトリ
 ```
 .
 ├── src
-│   ├── lang
-│   │   └── *.csv // 翻訳データ
 │   ├── pug // _(アンダーバー)つきは、ビルド対象にされないファイル。
-│   │   ├── _data
-│   │   │   ├── lang
-│   │   │   │   └── *.json
-│   │   │   ├── languages.js // トップの言語一覧
-│   │   │   └── pages.js // pugの書き出し設定
 │   │   ├── _layouts
 │   │   ├── _mixins
 │   │   ├── _template
@@ -57,20 +42,6 @@ VSCode
 │   └── config.json // ビルド時に保持しておくべき設定
 └── 設定ファイル各種
 ```
-
-## ブランチルール
-
-|ブランチ  |説明  |
-|---------|---------|
-|master     | FIXしたデータ |
-|develop     | 確認環境 |
-|feature/{内容}| 作業ブランチ |
-
-### 流れ
-1. masterからfeatureブランチを作成
-2. featureブランチで作業
-3. 作業後、featureからdevelopにマージしてテストアップ
-4. 確認後、featureからmasterにマージ
 
 ## 命名規則
 ### クラス名
@@ -112,51 +83,10 @@ CSSで使用する色は`_variables.scss`に変数宣言する。
 - og OGP画像
 - image 上記に当てはまらない画像全般
 
-## src/pug/_templatesの書き出し
-src/pug/_templates の pugファイルは、
-src/pug/_data/pages.jsに記述した内容に基づいて書き出されます。
-
-## コマンドなど
-### 準備
-```
-yarn install
-```
-
-### VSCodeの設定
-以下の拡張機能をインストールすることを推奨します。
-
-- ESLint
-- stylelint
-- Code Spell Checker
-- Name That Color
-
-.vscode/settings.jsonにLintのオートフィックス設定を記述してあります。
-
 ### 開発環境サーバー
-pug, scss, jsの変更をウォッチしながらローカルサーバーを起動する。
-```
-yarn run watch
+ejs, scss, jsの変更をウォッチしながらローカルサーバーを起動する。
 ```
 
 ### ビルド
-pug, scss, jsがビルドされる。
+ejs, scss, jsがビルドされる。
 ```
-yarn run build
-```
-
-### 本番ビルド
-scss, jsをビルドし、内容に基づいてハッシュを保存する。
-pugのビルド時にキャッシュ対策のクエリを付与する。
-```
-yarn run production
-```
-
-- クエリは `src/pug/_data/config/cachebustMappings.json` に出力されます。
-
-## 検証範囲
-- Google Chrome 最新版
-- Internet Explorer 11
-- Microsoft Edge 最新版
-- Safari 最新版
-- Mozilla Firefox 最新版
-- Opera 最新版
